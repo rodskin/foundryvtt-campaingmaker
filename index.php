159a0dd6b510e7a20453dd7b7faa500e4f05e784
fgilles <fgilles@ds-agency.com>
2020-04-14 11:40:03 +0200

    test avec download zip

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
-	ini_set(default_charset, "UTF8");
+	ini_set('default_charset', "UTF8");
 	setlocale(LC_ALL, 'en_US.UTF8');
 	
     if (isset($_POST) && !empty($_POST)) {
-        print_r($_POST);
+        //print_r($_POST);
 		$options = $_POST;
         $uniq_id = uniqid('', true);
-        echo $uniq_id;
+        //echo $uniq_id;
         // module creation
-		$tmp_folder = 'tmp/' . $uniq_id;
+		$tmp_folder = '/tmp/' . $uniq_id;
         mkdir($tmp_folder);
 		
         $module_slug = strtolower(preg_replace(array('/[^a-zA-Z0-9 -]/', '/[ -]+/', '/^-|-$/'), array(' ', '-', ''), remove_accents($_POST['campaign_name'])));
-		echo $module_slug;
+		//echo $module_slug;
 		$options['module_slug'] = $module_slug;
 		$module_path = $tmp_folder . '/' . $module_slug;
 		mkdir($module_path);
@@ -35,9 +35,20 @@
 		
 		file_put_contents($module_path . '/module.json', $module_file);
 		
+		$zip_name = '/tmp/' . $uniq_id . '.zip';
+		zip($tmp_folder . '/', $zip_name);
+		
+		//echo filesize('/tmp/' . $uniq_id . '.zip');
+		header('Content-Type: application/zip');
+		header('Content-Disposition: attachment; filename="' . basename($zip_name) . '"');
+		header('Content-Length: ' . filesize($zip_name));
+
+		flush();
+		@readfile($zip_name);
+		rrmdir($tmp_folder);
+		unlink($zip_name);
 		
     }
-
 ?>
  <!DOCTYPE html>
 <html>
@@ -51,9 +62,9 @@
         <div>
             <form action="" id="campaign_maker_form" method="post">
                 <input type="text" name="creator_name" placeholder="Insert your name *" required="required" /><br />
-                <input type="text" name="creator_url" placeholder="Insert your site url" /><br />
+                <input type="text" name="creator_url" placeholder="Insert your site url *" required="required" /><br />
                 <input type="text" name="campaign_name" placeholder="Insert your campaign name *" required="required" /><br />
-				<textarea name="campaign_description" placeholder="campaign description"></textarea>
+				<textarea name="campaign_description" placeholder="campaign description *" required="required"></textarea>
                 <br />
                 <input type="submit" value="create and download module" />
             </form>
@@ -100,14 +111,14 @@
 	
 	
 	function create_module_file($options) {
-		$module_file='
-		{   
+		$module_file='{   
    "name": "' . $options['module_slug'] . '",   
    "title": "' . $options['campaign_name'] . '",   
-   "description": "' . $options['campaign_name'] . '",   
-   "author": "' . $options['campaign_name'] . '",
+   "description": "' . $options['campaign_description'] . '",   
+   "author": "' . $options['creator_name'] . '",
    "version": "1.0.0",
    "minimumCoreVersion": "0.5.3",
+   "url": "' . $options['creator_url'] . '",
    "packs": [
     {
       "name": "equipment",
@@ -176,6 +187,62 @@
  }';
 		return $module_file;
 	}
+	
+	function zip($source, $destination){
+		if (!extension_loaded('zip') || !file_exists($source)) {
+			return false;
+		}
+
+		$zip = new ZipArchive();
+		if (!$zip->open($destination, ZIPARCHIVE::CREATE)) {
+			return false;
+		}
+
+		$source = str_replace('\\', '/', realpath($source));
+
+		if (is_dir($source) === true) {
+			$files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+
+			foreach ($files as $file) {
+				$file = str_replace('\\', '/', $file);
+
+				// Ignore "." and ".." folders
+				if (in_array(substr($file, strrpos($file, '/')+1), array('.', '..'))) {
+					continue;
+				}               
+
+				$file = realpath($file);
+
+				if (is_dir($file) === true) {
+					$zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
+				} elseif (is_file($file) === true) {
+					$zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
+				}
+			}
+		} elseif (is_file($source) === true) {
+			$zip->addFromString(basename($source), file_get_contents($source));
+		}
+
+		return $zip->close();
+	}
+	
+	function rrmdir($src) {
+		$dir = opendir($src);
+		while(false !== ( $file = readdir($dir)) ) {
+			if (( $file != '.' ) && ( $file != '..' )) {
+				$full = $src . '/' . $file;
+				if ( is_dir($full) ) {
+					rrmdir($full);
+				}
+				else {
+					unlink($full);
+				}
+			}
+		}
+		closedir($dir);
+		rmdir($src);
+	}
+
 
 
 ?>
